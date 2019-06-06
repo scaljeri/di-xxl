79b193136ccfd4d3d1f30bc04e1738cea8f2a8b4
Lucas Calje <lucas@calje.eu>
2019-06-06 23:24:27 +0200

    Included included and excluded options

--- scripts/di.ts
+++ scripts/di.ts
@@ -30,8 +30,8 @@ const args = yargs(mainArgs)
         demand: false,
         description: 'Enable debug messages'
     })
-    .option('entry', {
-        alias: 'e',
+    .option('file', {
+        alias: 'f',
         type: 'string',
         demand: false,
         description: "Entry filename"
@@ -48,10 +48,22 @@ const args = yargs(mainArgs)
         demand: false,
         description: 'Output file relative to `base. Defaults to `<entryfile>`-di.ts`'
     })
-    .example(`$0 -b ./src -e index.ts -p \'**/*.ts\' -o out.ts`, '-- Builds new file with injected code')
-    .example(`$0 -c -b ./src -e index.ts -p \'**/*.ts\' -o out.ts`, '-- Compiles all code with `tsc`')
+    .option('include', {
+        alias: 'i',
+        type: 'string',
+        demand: false,
+        description: 'List of paths, files or classes to include'
+    })
+    .option('exclude', {
+        alias: 'e',
+        type: 'string',
+        demand: false,
+        description: 'List of paths, files or classes to exclude'
+    })
+    .example(`$0 -b ./src -f index.ts -p \'**/*.ts\' -o out.ts`, '-- Builds new file with injected code')
+    .example(`$0 -c -b ./src -f index.ts -p \'**/*.ts\' -o out.ts`, '-- Compiles all code with `tsc`')
     .example(`$0 -c 'yarn build' -b ./src -e index.ts -o out.ts`, '-- Run command `yarn build`')
-    .example(`$0 -c yarn -b ./src -e index.ts -o out.ts -- build`, '-- Same as above')
+    .example(`$0 -c yarn -b ./src -f index.ts -o out.ts -- build`, '-- Same as above')
     .example(`$0 -b ./src index.ts -- --thread 10`, '-- Runs `ts-node ./src/index-di.ts --thread 10`')
     .wrap(130) // yargs.terminalWidth())
     .argv;
@@ -63,14 +75,16 @@ const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 const base = args.base;
-const entry = args.entry; 
+const file = args.file;
 const pattern = args.pattern;
 const outfile = args.outfile as string;
 const command = args.command as string;
 const debug = args.debug;
+const includes = (args.include || '').split(/,|\s+/g);
+const excludes = (args.exclude || '').split(/,|\s+/g);
 
-if (!fs.existsSync(entry)) {
-    console.log(`Ooops, input file '${path.join(base, entry)}' does not exist`);
+if (!fs.existsSync(file)) {
+    console.log(`Ooops, input file '${path.join(base, file)}' does not exist`);
     process.exit(0);
 }
 
@@ -91,7 +105,7 @@ function runCommand(): Promise<void> {
     [cmd, ...opts] = command.split(' ');
 
     const tsNode = spawn(cmd, [...opts, ...subArgs]);
-    log(`Running: ${cmd} ${opts.join(' ')} ${subArgs.join(' ')}`);
+    log(`Running: ${cmd} ${[...opts, ...subArgs].join(' ')}`);
 
     return new Promise(r => {
         tsNode.on('close', (code) => {
@@ -115,11 +129,11 @@ function inject(): Promise<void> {
     return new Promise(resolve => {
         log(`Glob file search: ${basePattern}`);
         glob(basePattern, {}, async (er, files) => {
-            let entryContent = await readFile(entry, 'utf8');
-            let start = path.relative(base, path.dirname(entry)) ; //args.base ? path.dirname(path.relative(args.base, './src/' + entry)): path.dirname(entry);
+            let entryContent = await readFile(file, 'utf8');
+            let start = path.relative(base, path.dirname(file)); //args.base ? path.dirname(path.relative(args.base, './src/' + entry)): path.dirname(entry);
 
             for (const file of files) {
-                if (path.join(base, entry) !== file.replace(/^\.\//, '')) {
+                if (path.join(base, file) !== file.replace(/^\.\//, '')) {
                     const name = path.relative(base, file).replace('.ts', '');
                     const re = new RegExp(`(from ['"]${name}['"]);`, 'm');
                     const contents = await readFile(file, 'utf8');
@@ -128,14 +142,35 @@ function inject(): Promise<void> {
                         if (entryContent.match(re)) {
                             entryContent.replace(re, `${RegExp.$1}${name}`)
                         } else {
-                            log(`Injecting ${file}`);
                             const className = contents.match(/export class ([^ ]+)/)[1];
 
                             let pathTo = path.relative(start, name);
                             if (!pathTo.match(/^\./)) {
                                 pathTo = `./${pathTo}`;
                             }
-                            entryContent = `import { ${className} } from '${pathTo}';${className};\n` + entryContent;
+                            let include = true;
+                            let exclude = false;
+
+                            if (includes.length > 0) {
+                                include = includes.includes(className) || includes.filter(i => pathTo.match(i)).length > 0;
+                            }
+
+                            if (excludes.length > 0) {
+                                exclude = excludes.includes(className) || 
+                                ( 
+                                    // excludes.filter(i => pathTo.match(i)).length > 0 ||
+                                    excludes.filter(i => file.match(i)).length > 0
+                                )
+
+                                if (exclude) {
+                                    log(`Exclude ${file}`);
+                                }
+                            }
+
+                            if (include && !exclude) {
+                                log(`Adding ${file}`);
+                                entryContent = `import { ${className} } from '${pathTo}';${className};\n` + entryContent;
+                            }
                         }
                     }
                 }
@@ -182,42 +217,47 @@ function getSubArguments(): string[] {
 }
 
 function sanitizeInput(yargs: Argv<{}>): void {
-    if (!args.entry) {
+    if (!args.file) {
         const pargs = mainArgs.slice(2);
         const last = pargs.slice(-1)[0] as string;
         const option = pargs.length > 1 ? pargs.slice(-2)[0] : null;
 
         if (last) {
             if (fs.existsSync(path.join(args.base || '', last))) {
-                args.entry = last;
+                args.file = last;
             } else if (fs.existsSync(last)) {
-                args.entry = path.relative(args.base, last);
+                args.file = path.relative(args.base, last);
             }
 
-            if (args.entry) {
+            if (args.file) {
                 if (option && option.match(/^-(d|-debug)/)) {
                     args.debug = true;
                 } else if (option && option.match(/^-(c|-compile)/)) {
                     args.compile = true;
                 }
             }
+
+            if (args.file && args.command === last) {
+                args.command = true;
+            }
         }
 
-        if (!args.entry) {
+        if (!args.file) {
             yargs.showHelp();
             process.exit(0);
         }
 
-        args.base = args.base || path.dirname(args.entry) || '.';
-        args.entry = fs.existsSync(path.join(args.base, args.entry)) ? path.join(args.base, args.entry) : args.entry;
+        args.base = args.base || path.dirname(args.file) || '.';
+        args.file = fs.existsSync(path.join(args.base, args.file)) ? path.join(args.base, args.file) : args.file;
 
-        // path.dirname(outifle) === path.dirname(entry) !! otherwise imports will break
-        args.outfile = args.outfile ? path.join(path.dirname(args.entry), path.basename(args.outfile as string)) : `${args.entry.replace(/\.ts/, '')}-di.ts`;
+        // path.dirname(outifle) === path.dirname(file) !! otherwise imports will break
+        args.outfile = args.outfile ? path.join(path.dirname(args.file), path.basename(args.outfile as string)) : `${args.file.replace(/\.ts/, '')}-di.ts`;
 
         if (args.command === undefined) {
             args.command = 'ts-node';
             subArgs.unshift(args.outfile as string);
         }
+
         if (args.command === true) {
             args.command = 'tsc';
         }
